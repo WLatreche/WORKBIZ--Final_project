1b260728cf18083a52fd216637550510744a34ba
Walid Latreche <walid@latreche.net>
2022-11-24 12:47:28 -0500

    Walid Latreche-1st commit

---
+++ server/data.js
@@ -0,0 +1,10 @@
+const users = [
+    {
+        givenName: "Eric",
+        surname: "Jameson",
+        work: "Plumber",
+        email: "e.jameson@gmail.com",
+        score: "4.5",
+        Projects: "4"
+    }
+]
